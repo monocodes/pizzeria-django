f37f8185b544c276389bd22370559bd4ba1d3c15
wandering-mono <locobu@icloud.com>
2022-12-22 19:51:15 +0400

    Define Topping model. - register in admin.py Topping model - don't update db!!!

--- pizzas/models.py
+++ pizzas/models.py
@@ -9,3 +9,12 @@ class Pizza(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return self.name
+
+class Topping(models.Model):
+    """Pizza toppings that belongs to pizzas."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.name
