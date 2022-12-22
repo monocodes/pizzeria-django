53b1992c28b57df2f1661b920cafd2a85e25fe8b
wandering-mono <locobu@icloud.com>
2022-12-22 21:31:33 +0400

    Change Topping fields behavior to select multiple toppings for pizzas - add to Pizza topping var and use models.ManyToManyField class for Topping - https://github.com/davrad/pythoncrashcourse/blob/master/chapter_18/pizzeria/pizzas/models.py

--- pizzas/models.py
+++ pizzas/models.py
@@ -2,18 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-class Pizza(models.Model):
-    """A pizza the user can choose from."""
+class Topping(models.Model):
+    """Pizza toppings that belongs to pizzas."""
+    #pizza = models.ForeignKey(Pizza, on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=100)
 
     def __str__(self):
         """Return a string representation of the model."""
         return self.name
 
-class Topping(models.Model):
-    """Pizza toppings that belongs to pizzas."""
-    pizza = models.ForeignKey(Pizza, on_delete=models.SET_NULL,null=True)
+class Pizza(models.Model):
+    """A pizza the user can choose from."""
     name = models.CharField(max_length=100)
+    topping = models.ManyToManyField(Topping, blank=True)
 
     def __str__(self):
         """Return a string representation of the model."""
