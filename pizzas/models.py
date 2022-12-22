1b7189da7a912937f40fdb1e073f1f0661c29a1f
wanderingMono <locobu@icloud.com>
2022-12-22 17:42:26 +0400

    Define model Pizza.
    - activate model
    - modify db

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Pizza(models.Model):
+    """A pizza the user can choose."""
+    name = models.CharField(max_length=100)
+
+    def __srt__(self):
+        """Return a string representation of the model."""
+        return self.name
