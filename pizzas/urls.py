8706ecdff1652b8410544329d238795f57cd03eb
wandering-mono <locobu@icloud.com>
2023-01-02 16:17:56 +0400

    18-8. Pizzeria Pages. - The Pizzas Page - The Pizzas URL Pattern - The Pizzas View - The Pizzas Template
    Individual Pizza Pages.
    - The Pizza URL Pattern
    - The Pizza View
    - The Pizza Template
    - Links from the Pizzas Page

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Page that shows all pizzas.
     path('pizza_types/', views.pizza_types, name='pizza_types'),
+    # Detail page for a single pizza.
+    path('pizza_types/<int:pizza_type_id>/', views.pizza_type, name='pizza_type'),
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -13,3 +13,10 @@ def pizza_types(request):
     pizza_types = Pizza.objects.order_by('name')
     context = {'pizza_types': pizza_types}
     return render(request, 'pizzas/pizza_types.html', context)
+
+def pizza_type(request, pizza_type_id):
+    """Show a single pizza type and all its toppings."""
+    pizza_type = Pizza.objects.get(id=pizza_type_id)
+    toppings = pizza_type.topping.all()
+    context = {'pizza_type': pizza_type, 'toppings': toppings}
+    return render(request, 'pizzas/pizza_type.html', context)
